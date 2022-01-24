35257756ba2fa16b816ce24bfb82e32db9154ca8
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-24 15:31:00 -0500

    basic set up and deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Group 10 is working on the final App</h1>
+    </div>
+  );
+}
+
+export default App;
