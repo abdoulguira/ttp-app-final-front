d66ad3f3793d9bf384c5ef08b7e03842b7f57a20
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-26 16:04:32 -0500

    put search bar on homepage instead of app

--- src/App.js
+++ src/App.js
@@ -8,142 +8,9 @@ import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 import './App.css';
 
-const sampleData = [{
-  "adult": false,
-  "backdrop_path": "/8xt8AMb1OKC63AdhNSaYBWxB4Iq.jpg",
-  "genre_ids": [
-              18,
-              53,
-              9648
-  ],
-  "id": 11324,
-  "original_language": "en",
-  "original_title": "Shutter Island",
-  "overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
-  "popularity": 53.829,
-  "poster_path": "/52d7CAjnwh1cZXd2lhKaHVFMu0Q.jpg",
-  "release_date": "2010-02-14",
-  "title": "Shutter Island",
-  "video": false,
-  "vote_average": 8.2,
-  "vote_count": 19257
-  },
-  {
-      "adult": false,
-      "backdrop_path": "/8xt8AMb1OKC63AdhNSaYBWxB4Iq.jpg",
-      "genre_ids": [
-                  18,
-                  53,
-                  9648
-      ],
-      "id": 11325,
-      "original_language": "en",
-      "original_title": "Shutter Island",
-      "overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
-      "popularity": 53.829,
-      "poster_path": "/52d7CAjnwh1cZXd2lhKaHVFMu0Q.jpg",
-      "release_date": "2010-02-14",
-      "title": "Shutter Island",
-      "video": false,
-      "vote_average": 8.2,
-      "vote_count": 19257
-      },
-      {
-          "adult": false,
-          "backdrop_path": "/8xt8AMb1OKC63AdhNSaYBWxB4Iq.jpg",
-          "genre_ids": [
-                      18,
-                      53,
-                      9648
-          ],
-          "id": 11326,
-          "original_language": "en",
-          "original_title": "Shutter Island",
-          "overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
-          "popularity": 53.829,
-          "poster_path": "/52d7CAjnwh1cZXd2lhKaHVFMu0Q.jpg",
-          "release_date": "2010-02-14",
-          "title": "Shutter Island",
-          "video": false,
-          "vote_average": 8.2,
-          "vote_count": 19257
-          },
-          {
-              "adult": false,
-              "backdrop_path": "/8xt8AMb1OKC63AdhNSaYBWxB4Iq.jpg",
-              "genre_ids": [
-                          18,
-                          53,
-                          9648
-              ],
-              "id": 11327,
-              "original_language": "en",
-              "original_title": "Shutter Island",
-              "overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
-              "popularity": 53.829,
-              "poster_path": "/52d7CAjnwh1cZXd2lhKaHVFMu0Q.jpg",
-              "release_date": "2010-02-14",
-              "title": "Shutter Island",
-              "video": false,
-              "vote_average": 8.2,
-              "vote_count": 19257
-              },
-              {
-                  "adult": false,
-                  "backdrop_path": "/8xt8AMb1OKC63AdhNSaYBWxB4Iq.jpg",
-                  "genre_ids": [
-                              18,
-                              53,
-                              9648
-                  ],
-                  "id": 11328,
-                  "original_language": "en",
-                  "original_title": "Shutter Island",
-                  "overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
-                  "popularity": 53.829,
-                  "poster_path": "/52d7CAjnwh1cZXd2lhKaHVFMu0Q.jpg",
-                  "release_date": "2010-02-14",
-                  "title": "Shutter Island",
-                  "video": false,
-                  "vote_average": 8.2,
-                  "vote_count": 19257
-                  },
-                  {
-                      "adult": false,
-                      "backdrop_path": "/8xt8AMb1OKC63AdhNSaYBWxB4Iq.jpg",
-                      "genre_ids": [
-                                  18,
-                                  53,
-                                  9648
-                      ],
-                      "id": 11329,
-                      "original_language": "en",
-                      "original_title": "Shutter Island",
-                      "overview": "World War II soldier-turned-U.S. Marshal Teddy Daniels investigates the disappearance of a patient from a hospital for the criminally insane, but his efforts are compromised by his troubling visions and also by a mysterious doctor.",
-                      "popularity": 53.829,
-                      "poster_path": "/52d7CAjnwh1cZXd2lhKaHVFMu0Q.jpg",
-                      "release_date": "2010-02-14",
-                      "title": "Shutter Island",
-                      "video": false,
-                      "vote_average": 8.2,
-                      "vote_count": 19257
-                      },
-]
-
-const api = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=`
-// need to link to backend with cors
-
 function App() {
-  const [movieData, setMovieData] = useState([])
-  const [searchTerm, setSearchTerm] = useState("smart")
 
-  useEffect(() => {
-    fetch(api + searchTerm)
-    .then(response => response.json())
-    .then(data => setMovieData(data['results']));
-  }, [searchTerm])
-
-  const HomePageElm = () => (<HomePage movieData={movieData}/>)
+  const HomePageElm = () => (<HomePage />)
   const MovieDetailPageElm = () => (<MovieDetailPage />)
   const LogInElm = () => (<LogIn />)
   const SignUpElm = () => (<SignUp />)
@@ -153,7 +20,6 @@ function App() {
           <div className="App">
             {/* search bar, log in, filter, goes here */}
             <NavBarBefore />
-            <SearchBar setSearchTerm={setSearchTerm}/>
             <Routes>
               <Route exact path="/" element={<HomePageElm/>}/>
               <Route exact path="/movie/:id" element={<MovieDetailPageElm/>}/>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from 'react';
 import MovieContainer from "./MovieContainer"
+import SearchBar from './SearchBar'
 
-export default function HomePage(props){
+const api = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=`
+// need to link to backend with cors
+
+export default function HomePage(){
+    const [movieData, setMovieData] = useState([])
+    const [searchTerm, setSearchTerm] = useState("smart")
+
+    useEffect(() => {
+        fetch(api + searchTerm)
+        .then(response => response.json())
+        .then(data => setMovieData(data['results']));
+    }, [searchTerm])
+
     return (
         <div>
-            <MovieContainer movieData={props.movieData}/>
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <MovieContainer movieData={movieData}/>
         </div>
 
     )
