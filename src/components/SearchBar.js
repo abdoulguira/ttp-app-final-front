59e81f35c9d168364983ec498b99c38a6cba2176
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-26 14:27:09 -0500

    search bar now works but needs to display movies

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,15 @@ import React, {useEffect, useState} from 'react';
 export default function SearchBar() {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
   const handleOnSumbit = (e) => {
     e.preventDefault();
-
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results) //results need to be displayed
+      });
   }
 
   const handleOnChange = (e) => {
