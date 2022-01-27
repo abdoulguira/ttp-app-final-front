08f8a8eab96be3c566c86548d1e0a6143dd19be0
KanG98 <kylexue@Yankangs-MBP.fios-router.home>
2022-01-27 16:34:02 -0500

    linked login and create user page to backend"

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,36 @@
 import React, {useState} from "react";
 
+const userAPI = "http://localhost:8001/users/"
+
 export default function LogIn() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    // const [logInSuccess, setLoginSuceess] =  navigate ... 
+
+
+    function handleUsername(e){
+        setUsername(e.target.value)
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(username)
+        console.log(password)
+        fetch(userAPI+username+'/'+password)
+            .then(res => {
+                if(res.status === 404)
+                    console.log("no user found")
+                if(res.status === 403)
+                    console.log("wrong password")
+                if(res.status === 200)
+                    console.log("logging successfully")
+            })
+    }
 
     return (
         <div className="login-page">
@@ -11,14 +41,16 @@ export default function LogIn() {
                         className='input' 
                         id='username' 
                         type='text' 
+                        onChange={handleUsername}
                     /> <br></br>
                     <label className='label'>Password: </label>
                     <input 
                         className='input' 
                         id='password' 
                         type='password' 
+                        onChange={handlePassword}
                     /> <br></br>
-                    <input type='submit' value='Login'></input>
+                    <input type='submit' value='Login' onClick={handleSubmit}></input>
                 </div>
         </div>
     );
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,60 @@
 import React, {useState} from "react";
 
+const newUserAPI = "http://localhost:8001/users/"
 export default function SignUp() {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [gender, setGender] = useState("M")
+
+    function handleUsername(e){
+        setUsername(e.target.value)
+    }
+
+    function handleEmail(e){
+        setEmail(e.target.value)
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value)
+    }
+    
+    function handleGender(e){
+        setGender(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        if (username.length < 5){
+            alert("username need to be at least 5 characters")
+        }
+        if (password.length < 8){
+            alert("password need to be at least 8 characters")
+        }
+        fetch(newUserAPI, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "username": username,
+                "user_password": password,
+                "email": email,
+                "gender": gender
+            })
+        })
+        .then( res =>{
+            if(res.status === 400){
+                alert('something went wrong, please try again')
+            }
+            else{
+                console.log("created user successfully")
+                // lead to main page
+            }
+        } 
+        )
+    }
 
     return (
         <div className="login-page">
@@ -11,12 +65,14 @@ export default function SignUp() {
                         className='input' 
                         id='username' 
                         type='text' 
+                        onChange={handleUsername}
                     /> <br></br>
                     <label className='label'>Email: </label>
                     <input 
                         className='input' 
                         id='email' 
                         type='email' 
+                        onChange={handleEmail}
 
                     /> <br></br>
                     <label className='label'>Password: </label>
@@ -24,8 +80,14 @@ export default function SignUp() {
                         className='input' 
                         id='password' 
                         type='password' 
+                        onChange={handlePassword}
                     /> <br></br>
-                    <input type='submit' value='Sign Up'></input>
+
+                    <select name="gender" id="gender" onChange={handleGender}>
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+    </select>
+                    <input type='submit' value='Sign Up' onClick={handleSubmit}></input>
                 </div>
         </div>
     );
